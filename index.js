15ba3bf8d731675b7996246d7341348dd1079752
zTitiK\tungthanh.1497 <tungthanh.1497@gmail.com>
2017-08-10 04:10:07 +0700

    update getFood

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongooseFood = require('mongoose');
+var mongoose = require('mongoose');
 var Food = require('./models/food');
-var mongooseUser = require('mongoose');
 var User = require('./models/user');
 var app = express();
 
-mongooseFood.connect('mongodb://admin:admin@ds161209.mlab.com:61209/cookmix_listfood'
-, {useMongoClient: true});
-mongooseUser.connect('mongodb://admin:admin@ds017205.mlab.com:17205/cookmix_userprofile'
+mongoose.connect('mongodb://admin:admin@ds161209.mlab.com:61209/cookmix_listfood'
 , {useMongoClient: true});
 
 
@@ -29,92 +26,7 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-app.post('/createUser', function(req, res){
-  var body = req.body;
-
-  // var idValue = body.id;
-  var idValue = body.idFb;
-  var avaValue = body.avaFb;
-  var nameValue=body.nameFb;
-  var emailValue = body.emailFb;
-  var ratePointValue = body.ratePoint;
-  var rateNumValue = body.rateNum;
-
-  var user = new User({
-      // id:idValue,
-      idFb: idValue,
-      avaFb: avaValue,
-      nameFb: nameValue,
-      emailFb: emailValue,
-      ratePoint: ratePointValue,
-      rateNum: rateNumValue
-    });
-
-
-  user.save(function(err, createdUser){
-      if(err){
-          res.json({"success":0, "message": "Could not add record: "+err});
-      }else {
-          res.json(createdUser);
-      }
-    }
-  );
-});
-app.get('/getFood', function(req, res){
-  Food.find(function(err, foods){
-    if(err){
-      res.json({success: 0, message: "Could not get data from mlab"});
-    }else {
-      // res.json(foods);
-      res.send(foods);
-    }
-  });
-});
-app.delete('/deleteFood/:foodId', function(req, res){
-    var foodId = req.params.foodId;
-
-    Food.findByIdAndRemove(foodId, function(error, food) {
-      if(err){
-        res.json({"success": 0, "message": "Could not delete data from mlab"});
-      }else {
-        res.json({"success": 1, "message": "Delete succesfully"});
-      }
-    });
-});
-app.put('/updateFood/:foodId', function(req, res){
-  Food.findById(req.params.foodId, function (err, food) {
-  // Handle any possible database errors
-    if (err) {
-        res.status(500).send(err);
-    } else {
-        // Update each attribute with any possible attribute that may have been submitted in the body of the request
-        // If that attribute isn't in the request body, default back to whatever it was before.
-
-
-        food.name = req.body.name || food.name;
-        food.author = req.body.author || food.author;
-        food.imageShow = req.body.imageShow || food.imageShow;
-        food.type = req.body.type || food.type;
-        food.time = req.body.time || food.time;
-        food.sets = req.body.sets || food.sets;
-        food.level = req.body.level || food.level;
-        food.rating = req.body.rating || food.rating;
-        food.material = req.body.material || food.material;
-        food.cook = req.body.cook || food.cook;
-
-        // Save the updated document back to the database
-        food.save(function (err, food) {
-            if (err) {
-                res.status(500).send(err)
-            }
-            res.send(food);
-        });
-    }
-  });
-});
-
-
-app.post('/createUser', function(req, res){
+app.post('/createFood', function(req, res){
   var body = req.body;
 
   // var idValue = body.id;
@@ -153,13 +65,137 @@ app.post('/createUser', function(req, res){
     }
   );
 });
-app.get('/getUser', function(req, res){
+app.get('/getFood', function(req, res){
+  Food.find(function(err, foods){
+    if(err){
+      res.json({success: 0, message: "Could not get data from mlab"});
+    }else {
+      // res.json(foods);
+      res.send({food:foods  });
+    }
+  });
+});
+app.put('/updateFood/:foodId', function(req, res){
+  Food.findById(req.params.foodId, function (err, food) {
+  // Handle any possible database errors
+    if (err) {
+        res.status(500).send(err);
+    } else {
+        // Update each attribute with any possible attribute that may have been submitted in the body of the request
+        // If that attribute isn't in the request body, default back to whatever it was before.
+
+
+        food.name = req.body.name || food.name;
+        food.author = req.body.author || food.author;
+        food.imageShow = req.body.imageShow || food.imageShow;
+        food.type = req.body.type || food.type;
+        food.time = req.body.time || food.time;
+        food.sets = req.body.sets || food.sets;
+        food.level = req.body.level || food.level;
+        food.rating = req.body.rating || food.rating;
+        food.material = req.body.material || food.material;
+        food.cook = req.body.cook || food.cook;
+
+        // Save the updated document back to the database
+        food.save(function (err, food) {
+            if (err) {
+                res.status(500).send(err)
+            }
+            res.send(food);
+        });
+    }
+  });
+});
+app.delete('/deleteFood/:foodId', function(req, res){
+    var foodId = req.params.foodId;
+
+    Food.findByIdAndRemove(foodId, function(error, food) {
+      if(err){
+        res.json({"success": 0, "message": "Could not delete data from mlab"});
+      }else {
+        res.json({"success": 1, "message": "Delete succesfully"});
+      }
+    });
+});
+
+
+app.post('/createUser', function(req, res){
+  var body = req.body;
+
+  // var idValue = body.id;
+  var idValue = body.idFb;
+  var avaValue = body.avaFb;
+  var nameValue=body.nameFb;
+  var emailValue = body.emailFb;
+  var ratePointValue = body.ratePoint;
+  var rateNumValue = body.rateNum;
+
+  var user = new User({
+      // id:idValue,
+      idFb: idValue,
+      avaFb: avaValue,
+      nameFb: nameValue,
+      emailFb: emailValue,
+      ratePoint: ratePointValue,
+      rateNum: rateNumValue
+    });
+
+
+  user.save(function(err, createdUser){
+      if(err){
+          res.json({"success":0, "message": "Could not add record: "+err});
+      }else {
+          res.json(createdUser);
+      }
+    }
+  );
+});
+app.get('/getAllUser', function(req, res){
   User.find(function(err, users){
     if(err){
       res.json({success: 0, message: "Could not get data from mlab"});
     }else {
       // res.json(foods);
       res.send(users);
+    }
+  });
+});
+app.get('/getUserProfile/:userId', function(req, res){
+  User.findOne({'/idFb/':req.params.userId}, function (err, user) {
+  // User.findById(req.params.userId,function(err, user){
+    if(err){
+      res.json({success: 0, message: "Could not get data from mlab"});
+    }else {
+      // res.json(foods);
+      res.send(user);
+    }
+  });
+});
+app.put('/updateUser/:userId', function(req, res){
+  // User.findById(req.params.userId, function (err, user) {
+  User.findOne({'/idFb/':req.params.userId}, function (err, user) {
+  // Handle any possible database errors
+    if (err) {
+        res.status(500).send(err);
+    } else {
+        // Update each attribute with any possible attribute that may have been submitted in the body of the request
+        // If that attribute isn't in the request body, default back to whatever it was before.
+
+        // user.idFb = req.body.idFb || user.idFb;
+        user.avaFb = req.body.avaFb || user.avaFb;
+        user.nameFb = req.body.nameFb || user.nameFb;
+        user.emailFb = req.body.emailFb || user.emailFb;
+        user.ratePoint = req.body.ratePoint || user.ratePoint;
+        user.rateNum = req.body.rateNum || user.rateNum;
+
+        // Save the updated document back to the database
+        user.save(function (err, user) {
+            if (err) {
+                res.status(500).send(err)
+            }else {
+              res.send(user);
+            }
+        });
     }
   });
 });
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var userSchema = new Schema({
-    idFb: Number,
+    idFb: String,
     avaFb: String,
     nameFb: String,
     emailFb: String,
